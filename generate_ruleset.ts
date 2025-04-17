519cbd06a2ebf8ffa20d7c1e396997b4633498c2
YUxiangLuo <871884617@qq.com>
2025-04-17 17:04:29 +0800

    fake ok

--- generate_ruleset.ts
+++ generate_ruleset.ts
@@ -2,6 +2,43 @@ import { sleep } from "bun";
 
 await generate_direct_list();
 await generate_reject_list();
+await generate_proxy_list();
+async function generate_proxy_list() {
+  const res = await fetch(
+    "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt",
+  );
+  const text = await res.text();
+
+  // difficult to type my data, so give any to it
+  const rule_set: any = {
+    rules: [
+      {
+        domain: [],
+        domain_suffix: [],
+        domain_regex: [],
+      },
+    ],
+    version: 3,
+  };
+
+  await Bun.write(import.meta.dir + "/rule/proxy-list.txt", text);
+  const items = text.split("\n");
+  for (const item of items) {
+    if (item.startsWith("full:")) {
+      rule_set.rules[0].domain.push(item.replace("full:", ""));
+    } else if (item.startsWith("regexp:")) {
+      rule_set.rules[0].domain_regex.push(item.replace("regexp:", ""));
+    } else {
+      if (item) rule_set.rules[0].domain_suffix.push(item);
+    }
+  }
+  await Bun.write(
+    import.meta.dir + "/rule/proxy-list.json",
+    JSON.stringify(rule_set),
+  );
+  sleep(1000);
+  await Bun.$`${import.meta.dir}/out/sing-box rule-set compile --output ${import.meta.dir}/rule/proxy.srs ${import.meta.dir}/rule/proxy-list.json`;
+}
 
 async function generate_reject_list() {
   const res = await fetch(
--- src/configs.ts
+++ src/configs.ts
@@ -1,115 +1,119 @@
 export const sing_box_config: any = {
-  log: {
-    disabled: false,
-    level: "info",
-    timestamp: true,
-  },
-  dns: {
-    strategy: "ipv4_only",
-    disable_cache: true,
-    final: "googledns",
-    servers: [
+
+  "log": { "disabled": false, "level": "info", "timestamp": true },
+  "dns": {
+    "final": "onedns",
+    "strategy": "ipv4_only",
+    "servers": [
       {
-        type: "udp",
-        tag: "googledns",
-        server: "8.8.8.8",
-        detour: "select",
+        "type": "udp",
+        "tag": "onedns",
+        "server": "1.1.1.1",
+        "detour": "select"
       },
+      { "type": "fakeip", "tag": "fakeip", "inet4_range": "198.18.0.0/15" },
       {
-        type: "udp",
-        tag: "alidns",
-        server: "223.5.5.5",
-        detour: "direct",
-      },
+        "tag": "alidns",
+        "type": "udp",
+        "server": "223.5.5.5",
+        "detour": "direct"
+      }
     ],
-    rules: [
+    "rules": [
       {
-        rule_set: ["china-site"],
-        action: "route",
-        server: "alidns",
+        "type": "logical",
+        "mode": "and",
+        "rules": [
+          { "rule_set": ["china-site"], "invert": true },
+          { "query_type": ["A", "AAAA"] }
+        ],
+        "action": "route",
+        "server": "fakeip"
       },
-    ],
+      {
+        "rule_set": ["china-site"],
+        "action": "route",
+        "server": "alidns"
+      }
+    ]
   },
-  inbounds: [
+  "inbounds": [
     {
-      type: "tun",
-      tag: "tun-in",
-      interface_name: "tun0",
-      address: ["172.18.0.1/30"],
-      mtu: 9000,
-      auto_route: true,
-      strict_route: true,
-      auto_redirect: true,
-    },
+      "type": "tun",
+      "tag": "tun-in",
+      "interface_name": "tun0",
+      "address": ["172.18.0.1/30"],
+      "mtu": 9000,
+      "auto_route": true,
+      "strict_route": true,
+      "auto_redirect": true
+    }
   ],
-  outbounds: [
+  "outbounds": [
     {
-      type: "selector",
-      tag: "select",
-      outbounds: [],
-      interrupt_exist_connections: true,
+      "type": "selector",
+      "tag": "select",
+      "outbounds": [
+      ],
+      "interrupt_exist_connections": true
     },
-    {
-      type: "direct",
-      tag: "direct",
-      domain_resolver: "alidns",
-    },
+    { "type": "direct", "tag": "direct", "domain_resolver": "alidns" },
   ],
-  experimental: {
-    clash_api: {
-      external_controller: "0.0.0.0:9090",
-      external_ui: "dashboard",
+  "experimental": {
+    "clash_api": {
+      "external_controller": "0.0.0.0:9090",
+      "external_ui": "dashboard"
+    },
+    "cache_file": {
+      "enabled": true,
+      "store_fakeip": true,
     },
   },
-  route: {
-    auto_detect_interface: true,
-    rules: [
+  "route": {
+    "final": "select",
+    "auto_detect_interface": true,
+    "rules": [
+      { "action": "sniff" },
+      { "protocol": "dns", "action": "hijack-dns" },
+      { "rule_set": ["ads-site"], "action": "reject" },
       {
-        action: "sniff",
+        "process_path": ["/usr/bin/qbittorrent"],
+        "action": "route",
+        "outbound": "direct"
       },
+      { "ip_is_private": true, "action": "route", "outbound": "direct" },
       {
-        protocol: "dns",
-        action: "hijack-dns",
-      },
-      {
-        rule_set: ["ads-site"],
-        action: "reject",
-      },
-      {
-        process_path: ["/usr/bin/qbittorrent"],
-        action: "route",
-        outbound: "direct",
-      },
-      {
-        ip_is_private: true,
-        action: "route",
-        outbound: "direct",
-      },
-      {
-        rule_set: ["china-ip", "china-site"],
-        action: "route",
-        outbound: "direct",
-      },
+        "rule_set": ["china-ip", "china-site"],
+        "action": "route",
+        "outbound": "direct"
+      }
     ],
-    rule_set: [
+    "rule_set": [
       {
-        type: "local",
-        tag: "china-site",
-        format: "binary",
-        path: "./rule/direct.srs",
+        "type": "local",
+        "tag": "china-site",
+        "format": "binary",
+        "path": "./rule/direct.srs"
       },
       {
-        type: "local",
-        tag: "ads-site",
-        format: "binary",
-        path: "./rule/reject.srs",
+        "type": "local",
+        "tag": "ads-site",
+        "format": "binary",
+        "path": "./rule/reject.srs"
       },
       {
-        type: "remote",
-        tag: "china-ip",
-        format: "binary",
-        url: "https://raw.githubusercontent.com/senshinya/singbox_ruleset/main/rule/ChinaIPs/ChinaIPs.srs",
+        "type": "local",
+        "tag": "proxy-site",
+        "format": "binary",
+        "path": "./rule/proxy.srs"
       },
-    ],
-  },
+      {
+        "type": "remote",
+        "tag": "china-ip",
+        "format": "binary",
+        "url": "https://raw.githubusercontent.com/senshinya/singbox_ruleset/main/rule/ChinaIPs/ChinaIPs.srs"
+      }
+    ]
+  }
+
 };
